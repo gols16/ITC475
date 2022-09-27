b061eb896124a5b3b1c8e803c75af648c80571cc
gols16 <100097021+gols16@users.noreply.github.com>
2022-09-26 23:26:36 -0500

    Update welcome.js

--- MegaTravel/js/welcome.js
+++ MegaTravel/js/welcome.js
@@ -1,1 +1,22 @@
+//display current date and time
+
+
+
+//visual icons
+// The visual icon will display a Sun between 6:00am and 6:00pm
+
+// The visual icon will display a Moon between 6:01pm and 5:59am
+
+
+
+
+// if statements 
+//If between 12am and 11:59am, the message should say "Good Morning"
+
+//if between 12pm and 5:00pm, the message should say "Good Afternoon"
+
+//if between 5:01pm and 11:59pm, the message should say "Good Evening" 
+
+
+
 
