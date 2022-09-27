4ba8f6da6e0b4f6c4ba6a963234edee7cd476e6b
gols16 <100097021+gols16@users.noreply.github.com>
2022-09-27 10:46:30 -0500

    Update welcome.js

--- MegaTravel/js/welcome.js
+++ MegaTravel/js/welcome.js
@@ -1,21 +1,41 @@
 //display current date and time
 
+function welcomeTime(){
 
+
+    
+}
+let now = new Date(); 
+let hours = now.getHours();
+let welcomeGreeting = document.getElementById('welcomeGreeting');
+let welcomeGreetingText = "";
+let welcomeIcon = document.getElementById('welcomeIcon');
 
 //visual icons
 // The visual icon will display a Sun between 6:00am and 6:00pm
+if(hours >= 6 && hours <= 18){
+
+}
+else{
+
 
 // The visual icon will display a Moon between 6:01pm and 5:59am
-
-
+}
 
 
 // if statements 
 //If between 12am and 11:59am, the message should say "Good Morning"
-
+if (hours >=0 && hours <=11){
+    welcomeGreetingText = "Good Morning!";
+}
 //if between 12pm and 5:00pm, the message should say "Good Afternoon"
-
+if (hours >=12 && hours <= 17){
+    welcomeGreetingText = "Good afternoon!";
+}
 //if between 5:01pm and 11:59pm, the message should say "Good Evening" 
+if (hours > 17 && hours <= 23){
+    welcomeGreetingText = " Good night!";
+}
 
 
 
