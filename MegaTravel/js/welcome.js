c0f11253fdf45b19d2dcbe5c45cd1ac6c1c0e1cb
gols16 <100097021+gols16@users.noreply.github.com>
2022-11-02 18:52:40 -0500

    Update welcome.js

--- MegaTravel/js/welcome.js
+++ MegaTravel/js/welcome.js
@@ -2,39 +2,45 @@
 
 function welcomeTime(){
 var display = new Date().toLocaleTimeString();
+var text = welcomeGreetingText;
 document.getElementById("currentTime").innerHTML=display;
+document.getElementById("welcomeGreetingText").innerHTML=text;
 }
+
 let now = new Date(); 
 let hours = now.getHours();
-let welcomeGreeting = document.getElementById('welcomeGreeting');
 let welcomeGreetingText = "";
 let welcomeIcon = document.getElementById('welcomeIcon');
 let currentTime= document.getElementById('currentTime');
+
 //visual icons
-// The visual icon will display a Sun between 6:00am and 6:00pm
+// display a Sun between 6:00am and 6:00pm
 if(hours >= 6 && hours <= 18){
     welcomeIcon.src = "sunIcon.jpeg";
 
 }
 else {
     welcomeIcon.src = "moonIcon.png";
-// The visual icon will display a Moon between 6:01pm and 5:59am
+//  display a Moon between 6:01pm and 5:59am
 }
 
 
 // if statements 
-//If between 12am and 11:59am, the message should say "Good Morning"
+// between 12am and 11:59am "Good Morning"
 if (hours >=0 && hours <=11){
-    welcomeGreetingText = "Good Morning!";
+    welcomeGreetingText = " Good Morning!";
 }
-//if between 12pm and 5:00pm, the message should say "Good Afternoon"
+
+// between 12pm and 5:00pm "Good Afternoon"
 if (hours >=12 && hours <= 17){
-    welcomeGreetingText = "Good afternoon!";
+    welcomeGreetingText = " Good afternoon!";
 }
-//if between 5:01pm and 11:59pm, the message should say "Good Evening" 
+
+// between 5:01pm and 11:59pm, "Good Evening" 
 if (hours > 17 && hours <= 23){
     welcomeGreetingText = " Good night!";
 }
+
 setInterval (welcomeTime,1000);
 
 
