3b4900425965f4ad2509d65ec317ac8341a7e1e8
Grace Olson <graceolson01@gmail.com>
2022-11-10 16:04:40 -0600

    deleted:    MegaTravel/admin.php 	deleted:    MegaTravel/confirm.php 	deleted:    MegaTravel/login.php

--- MegaTravel/admin.php
+++
@@ -1,1 +0,0 @@
-<!-- retrieves all forms stored in the MySQL database table.  You should loop through each stored form and display the information-->
--- MegaTravel/login.php
+++
@@ -1,4 +0,0 @@
-<!-- The page should check the credentials given in the form against a table in your database that stores usernames and passwords.
-If the user gives the correct credentials, they are sent to the admin page.
-If the user does not give the correct credentials, they should be shown an appropriate error message on the login page
--->
